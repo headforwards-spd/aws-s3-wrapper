a82fb75cbf71de2ed21c15edff08c4a1d526a02f
Andy Weir <andy.weir@headforwards.com>
2017-11-01 11:43:27 +0000

    add useage to readme

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# aws-s3-wrapper
+# AWS s3 Wrapper
+
+A handful of utility tools for AWS s3 functions
+
+http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+
+## Usage:
+
+```javascript
+const s3 = require('@headforwards-spd/aws-s3');
+```
+
+### saveDataUri()
+
+```javascript
+const s3Bucket  = 'my-s3-bucket';
+const dataUri   = 'my-data-uri';
+const objectKey = 'my-object-key';
+
+function saveMyDataUri() {
+
+    return new Promise((resolve, reject) => {
+        
+        try {
+
+            s3.saveDataUri(s3Bucket, dataUri, objectKey)
+                .then(
+                    response => resolve(response),
+                    error => reject(error)
+                );
+
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
+```
+
+### getObjectUrl()
+
+```javascript
+const s3Bucket  = 'my-s3-bucket';
+const objectKey = 'my-object-key';
+
+function myObjectGetUrlIs() {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            s3.getObjectUrl(s3Bucket, objectKey)
+                .then(
+                    getObjectUrl => resolve(getObjectUrl),
+                    error => reject(error)
+                );
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
+```
+
+### putObjectUrl()
+
+```javascript
+const s3Bucket    = 'my-s3-bucket';
+const objectKey   = 'my-object-key';
+const contentType = 'my-object-content-type';
+
+function myObjectPutUrlIs() {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            s3.putObjectUrl(s3Bucket, objectKey, contentType)
+                .then(
+                    putObjectUrl => resolve(putObjectUrl),
+                    error => reject(error)
+                );
+
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+const AWS = require('aws-sdk');
+const s3  = new AWS.S3();
+
+module.exports.saveDataUri  = saveDataUri;
+module.exports.getObjectUrl = getObjectUrl;
+module.exports.putObjectUrl = putObjectUrl;
+
+function saveDataUri(s3Bucket, dataUri, key) {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            const contentTypeRegExp = new RegExp(/^data:(.+);/);
+            const regex             = new RegExp(/^data:(?:.+);base64,(.*)$/);
+
+            const contentType = contentTypeRegExp.exec(dataUri)[1];
+            const data        = regex.exec(dataUri)[1];
+
+            const params = {
+                Bucket:      s3Bucket,
+                Key:         key,
+                ContentType: contentType,
+                Body:        new Buffer(data, 'base64')
+            };
+
+            // if signature filed exists
+            s3.putObject(params, (error, response) => {
+
+                    !error && resolve(response) || reject(error);
+                }
+            );
+
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
+
+function getObjectUrl(s3Bucket, key) {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            let params = {
+                Bucket: s3Bucket,
+                Key:    key
+            };
+
+            s3.headObject(params, (error) => {
+
+                let url = !error ? s3.getSignedUrl('getObject', params) : null;
+
+                resolve(url);
+            });
+
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
+
+function putObjectUrl(s3Bucket, key, contentType) {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            const params = {
+                Bucket:      s3Bucket,
+                Key:         key,
+                ContentType: contentType
+            };
+
+            let url = (!!contentType) ? s3.getSignedUrl('putObject', params) : null;
+
+            resolve(url);
+
+        } catch (error) {
+
+            reject(error);
+        }
+    });
+}
